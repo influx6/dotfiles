2f62358d8b3d6ed21df843fc0f4809d2e7adb393
Alexander Ewetumo <trinoxf@gmail.com>
2025-02-27 12:28:54 +0800

    UPT: neotree

--- nvim/lua/plugins/plugin-neo-tree.lua
+++ nvim/lua/plugins/plugin-neo-tree.lua
@@ -12,6 +12,39 @@ return {
       "MunifTanjim/nui.nvim",
       { "3rd/image.nvim", opts = {} },
     },
+    cmd = "Neotree",
+    keys = {
+      {
+        "<leader>fe",
+        function()
+          require("neo-tree.command").execute({ toggle = true, dir = LazyVim.root() })
+        end,
+        desc = "Explorer NeoTree (Root Dir)",
+      },
+      {
+        "<leader>fE",
+        function()
+          require("neo-tree.command").execute({ toggle = true, dir = vim.uv.cwd() })
+        end,
+        desc = "Explorer NeoTree (cwd)",
+      },
+      { "<leader>e", "<leader>fe", desc = "Explorer NeoTree (Root Dir)", remap = true },
+      { "<leader>E", "<leader>fE", desc = "Explorer NeoTree (cwd)", remap = true },
+      {
+        "<leader>ge",
+        function()
+          require("neo-tree.command").execute({ source = "git_status", toggle = true })
+        end,
+        desc = "Git Explorer",
+      },
+      {
+        "<leader>be",
+        function()
+          require("neo-tree.command").execute({ source = "buffers", toggle = true })
+        end,
+        desc = "Buffer Explorer",
+      },
+    },
     opts = {
       default_component_configs = {
         indent = {
