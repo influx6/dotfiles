a903a9a87e90988c22a35b55c7405ec52a2da507
Alexander Ewetumo <trinoxf@gmail.com>
2025-02-27 16:46:01 +0800

    FIX: neotree focus

--- nvim/lua/plugins/plugin-neo-tree.lua
+++ nvim/lua/plugins/plugin-neo-tree.lua
@@ -295,7 +295,7 @@ return {
         },
       },
       filesystem = {
-        bind_to_cwd = false,
+        bind_to_cwd = true,
         follow_current_file = { enabled = true },
         use_libuv_file_watcher = true,
         window = {
