54520b1310e597bcc55c0b7441cbe449c3d465f8
Alexander Ewetumo <trinoxf@gmail.com>
2025-05-04 20:01:05 +0800

    FIX: death of build run loop when error occurs

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -72,7 +72,7 @@ end, { desc = "Small terminal at the bottom" })
 
 map("n", "<leader>w15", function()
   vim.api.nvim_win_set_height(0, 15)
-end, { desc = "Small terminal at the bottom" })
+end, { desc = "Resize the buffer to height of 15" })
 
 -- better up/down
 map({ "n", "x" }, "j", "v:count == 0 ? 'gj' : 'j'", { desc = "Down", expr = true, silent = true })
