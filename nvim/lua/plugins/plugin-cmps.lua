740e50620912eec57e9a06c3dd20e771a750ead0
Alexander Ewetumo <trinoxf@gmail.com>
2025-05-05 18:34:53 +0800

    FIX: cmp ordering

--- nvim/lua/plugins/plugin-cmps.lua
+++ nvim/lua/plugins/plugin-cmps.lua
@@ -115,6 +115,27 @@ return {
       -- (Default) Only show the documentation popup when manually triggered
       completion = { documentation = { auto_show = true, auto_show_delay_ms = 70 } },
 
+      -- (Default) Rust fuzzy matcher for typo resistance and significantly better performance
+      -- You may use a lua implementation instead by using `implementation = "lua"` or fallback to the lua implementation,
+      -- when the Rust fuzzy matcher is not available, by using `implementation = "prefer_rust"`
+      --
+      -- See the fuzzy documentation for more information
+      fuzzy = {
+        implementation = "prefer_rust_with_warning",
+        sorts = {
+          function(a, b)
+            local sort = require("blink.cmp.fuzzy.sort")
+            if a.source_id == "spell" and b.source_id == "spell" then
+              return sort.label(a, b)
+            end
+          end,
+          -- This is the normal default order, which we fall back to
+          "score",
+          "kind",
+          "label",
+        },
+      },
+
       -- Signature: show function signatures (experimental)
       signature = { enabled = true },
 
@@ -489,27 +510,6 @@ return {
               },
             },
           },
-        },
-      },
-
-      -- (Default) Rust fuzzy matcher for typo resistance and significantly better performance
-      -- You may use a lua implementation instead by using `implementation = "lua"` or fallback to the lua implementation,
-      -- when the Rust fuzzy matcher is not available, by using `implementation = "prefer_rust"`
-      --
-      -- See the fuzzy documentation for more information
-      fuzzy = {
-        implementation = "prefer_rust_with_warning",
-        sorts = {
-          function(a, b)
-            local sort = require("blink.cmp.fuzzy.sort")
-            if a.source_id == "spell" and b.source_id == "spell" then
-              return sort.label(a, b)
-            end
-          end,
-          -- This is the normal default order, which we fall back to
-          "score",
-          "kind",
-          "label",
         },
       },
     },
