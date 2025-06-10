1ea7c401d1d28b6e5388f922edc52a6b69abed5c
Alexander Ewetumo <trinoxf@gmail.com>
2025-06-10 22:31:04 +0800

    ADD: disable avante

--- nvim/lua/plugins/plugin-ai.lua
+++ nvim/lua/plugins/plugin-ai.lua
@@ -282,73 +282,73 @@ return {
     end,
   },
 
-  {
-    "yetone/avante.nvim",
-    event = "VeryLazy",
-    version = false, -- Never set this value to "*"! Never!
-    opts = {
-      -- add any opts here
-      -- for example
-      -- provider = "openai",
-      -- openai = {
-      --   endpoint = "https://api.openai.com/v1",
-      --   model = "gpt-4o", -- your desired model (or use gpt-4o, etc.)
-      --   timeout = 30000, -- Timeout in milliseconds, increase this for reasoning models
-      --   temperature = 0,
-      --   max_tokens = 8192, -- Increase this to include reasoning tokens (for reasoning models)
-      --   --reasoning_effort = "medium", -- low|medium|high, only used for reasoning models
-      -- },
-      --
-      -- For Deepseek-r1
-      -- provider = "ollama",
-      -- ollama = {
-      --   model = "deepseek-r1:70b",
-      -- },
-      --
-      provider = "ollama",
-      ollama = {
-        -- model = "deepseek-r1:70b",
-        -- model = "qwq:latest", -- to use latest qwuen reasoning model
-        model = "gemma3:12b", -- to use latest google open source model
-      },
-    },
-    -- if you want to build from source then do `make BUILD_FROM_SOURCE=true`
-    build = "make",
-    -- build = "powershell -ExecutionPolicy Bypass -File Build.ps1 -BuildFromSource false" -- for windows
-    dependencies = {
-      "nvim-treesitter/nvim-treesitter",
-      "stevearc/dressing.nvim",
-      "nvim-lua/plenary.nvim",
-      "MunifTanjim/nui.nvim",
-      --- The below dependencies are optional,
-      "nvim-telescope/telescope.nvim", -- for file_selector provider telescope
-      "ibhagwan/fzf-lua", -- for file_selector provider fzf
-      "nvim-tree/nvim-web-devicons", -- or echasnovski/mini.icons
-      {
-        -- support for image pasting
-        "HakonHarnes/img-clip.nvim",
-        event = "VeryLazy",
-        opts = {
-          -- recommended settings
-          default = {
-            embed_image_as_base64 = false,
-            prompt_for_file_name = false,
-            drag_and_drop = {
-              insert_mode = true,
-            },
-            -- required for Windows users
-            use_absolute_path = true,
-          },
-        },
-      },
-      {
-        -- Make sure to set this up properly if you have lazy=true
-        "MeanderingProgrammer/render-markdown.nvim",
-        opts = {
-          file_types = { "markdown", "Avante" },
-        },
-        ft = { "markdown", "Avante" },
-      },
-    },
-  },
+  -- {
+  --   "yetone/avante.nvim",
+  --   event = "VeryLazy",
+  --   version = false, -- Never set this value to "*"! Never!
+  --   opts = {
+  --     -- add any opts here
+  --     -- for example
+  --     -- provider = "openai",
+  --     -- openai = {
+  --     --   endpoint = "https://api.openai.com/v1",
+  --     --   model = "gpt-4o", -- your desired model (or use gpt-4o, etc.)
+  --     --   timeout = 30000, -- Timeout in milliseconds, increase this for reasoning models
+  --     --   temperature = 0,
+  --     --   max_tokens = 8192, -- Increase this to include reasoning tokens (for reasoning models)
+  --     --   --reasoning_effort = "medium", -- low|medium|high, only used for reasoning models
+  --     -- },
+  --     --
+  --     -- For Deepseek-r1
+  --     -- provider = "ollama",
+  --     -- ollama = {
+  --     --   model = "deepseek-r1:70b",
+  --     -- },
+  --     --
+  --     provider = "ollama",
+  --     ollama = {
+  --       -- model = "deepseek-r1:70b",
+  --       -- model = "qwq:latest", -- to use latest qwuen reasoning model
+  --       model = "gemma3:12b", -- to use latest google open source model
+  --     },
+  --   },
+  --   -- if you want to build from source then do `make BUILD_FROM_SOURCE=true`
+  --   build = "make",
+  --   -- build = "powershell -ExecutionPolicy Bypass -File Build.ps1 -BuildFromSource false" -- for windows
+  --   dependencies = {
+  --     "nvim-treesitter/nvim-treesitter",
+  --     "stevearc/dressing.nvim",
+  --     "nvim-lua/plenary.nvim",
+  --     "MunifTanjim/nui.nvim",
+  --     --- The below dependencies are optional,
+  --     "nvim-telescope/telescope.nvim", -- for file_selector provider telescope
+  --     "ibhagwan/fzf-lua", -- for file_selector provider fzf
+  --     "nvim-tree/nvim-web-devicons", -- or echasnovski/mini.icons
+  --     {
+  --       -- support for image pasting
+  --       "HakonHarnes/img-clip.nvim",
+  --       event = "VeryLazy",
+  --       opts = {
+  --         -- recommended settings
+  --         default = {
+  --           embed_image_as_base64 = false,
+  --           prompt_for_file_name = false,
+  --           drag_and_drop = {
+  --             insert_mode = true,
+  --           },
+  --           -- required for Windows users
+  --           use_absolute_path = true,
+  --         },
+  --       },
+  --     },
+  --     {
+  --       -- Make sure to set this up properly if you have lazy=true
+  --       "MeanderingProgrammer/render-markdown.nvim",
+  --       opts = {
+  --         file_types = { "markdown", "Avante" },
+  --       },
+  --       ft = { "markdown", "Avante" },
+  --     },
+  --   },
+  -- },
 }
